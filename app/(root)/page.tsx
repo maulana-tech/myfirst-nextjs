21fd0d3abe88c6a4e04d6d24b5c3e2ef04e967c1
maulana-tech <firdausyah03@gmail.com>
2024-12-06 15:52:57 +0700

    menambah hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline"> Home </h1>
+    <section className="pink_container">
+       <h1 className="heading">Pitch Your Startup, <br/> Connect With Entrepeuner
+       </h1>
+
+       <p className="sub-heading !max-w-3-xl">
+        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+       </p>
+
+
+    </section>
+
     </>
   );
 }
